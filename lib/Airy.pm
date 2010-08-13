d7369d9ed8a4dd14ca643c931919346fa8d18d59
Craftworks <craftwork at cpan org>
2010-08-13 18:48:22 +0000

    Added basic files

---
+++ lib/Airy.pm
@@ -0,0 +1,25 @@
+package Airy;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+1;
+
+=head1 NAME
+
+Airy - The lightweight application framework
+
+=head1 AUTHOR
+
+Craftworks, C<< <craftwork at cpan org> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2010 Craftworks, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
