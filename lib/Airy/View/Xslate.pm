54ef2d2f108200ec58d229ff61910c3cefcf23cd
Craftworks <craftwork at cpan org>
2010-11-23 22:38:44 +0900

    added feature find __fucntion_xxx methos and set up functions

--- lib/Airy/View/Xslate.pm
+++ lib/Airy/View/Xslate.pm
@@ -3,6 +3,7 @@ package Airy::View::Xslate;
 use Airy;
 use base 'Airy::View';
 use Text::Xslate;
+use Class::Inspector;
 
 sub initialize {
     my $self = shift;
@@ -13,9 +14,26 @@ sub initialize {
 
     $self->{'tx'} = Text::Xslate->new(
         'path' => [ 'template' ],
+        'function' => $self->setup_function,
         %{ $self->config },
         @_,
     );
+}
+
+sub setup_function {
+    my $self = shift;
+
+    my $function;
+    my $methods = Class::Inspector->methods(ref $self, 'private', 'expanded');
+
+    no strict 'refs';
+    for my $method ( @$methods ) {
+        if ( $method->[2] =~ /^__function_(\w+)/o ) {
+            $function->{ $1 } = $method->[3];
+        }
+    }
+
+    return $function;
 }
 
 sub render {
